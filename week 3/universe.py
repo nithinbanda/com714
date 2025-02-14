e12225392ca9e88fe70336f126f6be9cb68b25fb
NITHIN BANDA <0bandn32@solent.ac.uk>
2025-02-14 14:56:40 +0000

    created a basic human class

---
+++ week 3/universe.py
@@ -0,0 +1,13 @@
+class Universe:
+
+ def __init__(self):
+     self.__planets = []
+
+ def generate(self) -> Planet:
+     planet = Planet()
+     self.__planets.append(planet)
+     return planet
+
+ def display(self) -> None:
+ for planet in self.__planets:
+ print(planet)
